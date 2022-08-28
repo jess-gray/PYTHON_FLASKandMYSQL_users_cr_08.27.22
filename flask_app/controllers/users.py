edfa0bab55b0212d24548bde82f2e86d5cbcd8a7
jess_gray <jac.cooper32@gmail.com>
2022-08-28 15:35:55 -0400

    CRUD added - still need to figure out how to make create user go to read one user page

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -2,16 +2,16 @@ from flask_app import app
 from flask import Flask, render_template, request, redirect, session 
 from flask_app.models.user import User
 
-@app.route('/users')
+@app.route('/users') #this is the page/form to add a new user
 def user():
     return render_template('create.html')
 
-@app.route('/users/new')
+@app.route('/users/new') #this is to show ALL users (also home)
 def route():
     all_users = User.get_all()
     return render_template('read_all.html', all_the_users = all_users)
 
-@app.route('/new_user', methods = ["POST"])
+@app.route('/new_user', methods = ["POST"]) #this is actually adding the new user
 def create_user():
     print(request.form)
     data = {
@@ -20,4 +20,39 @@ def create_user():
         'email': request.form['email']
     }
     User.create(data)
+    return redirect('users/new')
+
+@app.route('/read/<int:id>') #this is to read one users info
+def one_user(id):
+    data = {
+        'id': id
+    }
+    a_user = User.get_one(data)
+    return render_template('read_one.html', one_user = a_user)
+
+@app.route('/delete/<int:id>', methods = ["POST"]) #this is to delete user
+def delete(id):
+    data = {
+        'id' : id
+    }
+    User.delete(data)
     return redirect('/users/new')
+
+@app.route('/edit/<int:id>') #this is the edit page/form
+def edit(id):
+    data = {
+        'id' : id
+    }
+    a_user = User.get_one(data)
+    return render_template('edit.html', one_user = a_user)
+
+@app.route('/update/<int:id>', methods = ['POST']) #this is to actually edit the user
+def update(id):
+    data = {
+        'id' : id,
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email']
+    }
+    User.update(data)
+    return redirect(f'/read/{id}') #make sure to add f string - cannot just copy previous link
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -9,7 +9,7 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
-    @classmethod
+    @classmethod #this is to show all the users
     def get_all(cls):
         query = 'SELECT * FROM users;'
         results = connectToMySQL('cd_users').query_db(query)
@@ -19,9 +19,30 @@ class User:
             all_users.append(cls(one_user))
         return all_users
 
-    @classmethod
+    @classmethod #this is actually adding the new user
     def create(cls, data):
         query = 'INSERT INTO users (first_name, last_name, email) VALUES (%(first_name)s, %(last_name)s, %(email)s);'
+        results = connectToMySQL('cd_users').query_db(query, data)
+        print(results)
+        return results
+    
+    @classmethod #this is to read one users info
+    def get_one(cls, data):
+        query = 'SELECT * FROM users WHERE id = %(id)s;'
+        results = connectToMySQL('cd_users').query_db(query, data)
+        print(results)
+        return cls(results[0])
+
+    @classmethod #this is to delete user
+    def delete(cls, data):
+        query = 'DELETE FROM users WHERE id = %(id)s;'
+        results = connectToMySQL('cd_users').query_db(query, data)
+        print(results)
+        return results
+
+    @classmethod #this is to actually edit the user
+    def update(cls, data):
+        query = 'UPDATE users set first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(id)s;'
         results = connectToMySQL('cd_users').query_db(query, data)
         print(results)
         return results
